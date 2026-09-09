b8ee00a1698019abd2d12997c7847ad4e5288f3f
ARBM SIST Automation <arbm-sist@users.noreply.github.com>
2026-09-09 14:43:30 -0300

    promote third account-bound free capacity pool

--- test_resilience_capacity.py
+++ test_resilience_capacity.py
@@ -12,14 +12,14 @@ class ResilienceCapacityTests(unittest.TestCase):
     def test_current_3m_certificate_stays_green(self):
         r = certify_resilience(self.routes)
         self.assertEqual(r["base_3m_status"], "PASS")
-        self.assertEqual(r["certified_tokens_per_day"], 3_308_966)
+        self.assertEqual(r["certified_tokens_per_day"], 3_341_225)
 
     def test_current_mesh_is_not_n_plus_one(self):
         r = certify_resilience(self.routes)
-        self.assertEqual(r["n_plus_one_floor_tokens_per_day"], 967_741)
+        self.assertEqual(r["n_plus_one_floor_tokens_per_day"], 1_000_000)
         self.assertEqual(r["n_plus_one_3m_status"], "FAIL_INSUFFICIENT_CAPACITY")
 
     def test_growth_gate_is_fail_closed(self):
         r = certify_resilience(self.routes)
         self.assertEqual(r["growth_5m_status"], "FAIL_INSUFFICIENT_CAPACITY")
-        self.assertEqual(r["growth_5m_deficit_tokens_per_day"], 1_691_034)
+        self.assertEqual(r["growth_5m_deficit_tokens_per_day"], 1_658_775)
