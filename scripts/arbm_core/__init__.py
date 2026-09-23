a8174dff0a3d6ad8e2896ee22e1235c5c6227ce4
arbmsistone-lab <arbmsistone@gmail.com>
2026-09-23 09:46:00 -0300

    feat(vnext): export guarded executor adapters

--- scripts/arbm_core/__init__.py
+++ scripts/arbm_core/__init__.py
@@ -1,6 +1,7 @@
 """ARBM SIST vNEXT core architecture."""
 from .action_ir import ActionIR, ActionKind, TargetRef
 from .assurance import AssuranceDecision, AssurancePlane
+from .executor_adapter import CompiledAction, LegacyExecutorAdapter, LegacyObservationAdapter
 from .planner import HierarchicalPlanner, Mission, Plan
 from .recovery import RootCause, RootCauseClassifier
 from .runtime import ARBMRuntime, RuntimeResult
@@ -10,7 +11,7 @@ from .world_model import Evidence, SemanticEntity, WorldModel, fuse_world_state
 
 __all__ = [
     "ActionIR","ActionKind","TargetRef","AssuranceDecision","AssurancePlane",
-    "HierarchicalPlanner","Mission","Plan","RootCause","RootCauseClassifier",
+    "HierarchicalPlanner","Mission","Plan","CompiledAction","LegacyExecutorAdapter","LegacyObservationAdapter","RootCause","RootCauseClassifier",
     "ARBMRuntime","RuntimeResult","TransactionCoordinator","TransactionResult",
     "IndependentVerifier","VerificationResult","Evidence","SemanticEntity",
     "WorldModel","fuse_world_state",
